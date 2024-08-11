43547966f4a2c0f7f66e970a58a03a64c0b97fec
Yassir_3630 <koutoukale+GitOnVS@gmail.com>
2024-08-10 21:04:41 -0400

    Working on anonymous delegate

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,7 +4,9 @@ using DemoLibrary;
 
 PopulateCartWithDemoData();
 
-Console.WriteLine($"The total for the cart is {cart.GenerateTotal():C2}");
+Console.WriteLine($"The total for the cart is {cart.GenerateTotal(TellUser, CalculateSubTotal, DiscountInProcess):C2}");
+Console.WriteLine($"The total for the cart is {cart.GenerateTotal(x=>Console.WriteLine($"The subtotal is {x:C2}"), (x, y) => )}");
+
 
 Console.WriteLine();
 Console.Write("Please press any key to exit the application...");
@@ -18,6 +20,20 @@ static void PopulateCartWithDemoData()
     cart.Items.Add(new () { ItemName = "Blueberries", Price = 8.84M });
 }
 
+static decimal CalculateSubTotal(List<ProductModel> products, decimal subTotal)
+
+    => subTotal switch
+    {
+        > 100 => subTotal * 0.80M,
+        > 50 => subTotal * 0.85M,
+        > 10 => subTotal * 0.95M,
+        _ => subTotal
+    };
+
+static void DiscountInProcess(string message) => Console.WriteLine(message);
+
+
+static void TellUser(decimal price) => Console.WriteLine($"The subtotal is {price:C2}");
 public partial class Program
 {
     static ShoppingCartModel cart = new();
--- DemoLibrary/ShoppingCartModel.cs
+++ DemoLibrary/ShoppingCartModel.cs
@@ -4,26 +4,16 @@ namespace DemoLibrary;
 
 public class ShoppingCartModel
 {
+    public delegate void MentionDiscount(decimal subTotal);
     public List<ProductModel> Items { get; set; } = new();
 
-    public decimal GenerateTotal()
+    public decimal GenerateTotal(MentionDiscount alertUser,
+        Func<List<ProductModel>, decimal, decimal> getSubTotal,
+        Action<string> mentionDiscount)
     {
         decimal subTotal = Items.Sum(x => x.Price);
-        if (subTotal > 100)
-        {
-            return subTotal * 0.80M;
-        }
-        else if (subTotal > 50)
-        {
-            return subTotal * 0.85M;
-        }
-        else if (subTotal > 10)
-        {
-            return subTotal * 0.90M;
-        }
-        else
-        {
-            return subTotal;
-        }
+        alertUser(subTotal);
+        mentionDiscount("We are applying the discount.");
+        return getSubTotal(Items, subTotal);
     }
 }
